ed446777bdf3012f6f38be8203c62b401828b7b6
srhqmp <ballanosarah@gmail.com>
2021-06-17 11:18:10 +0800

    Implement user login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('express-async-errors')
 
 const booksRouter = require('./controllers/books')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -31,6 +32,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/books', booksRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
