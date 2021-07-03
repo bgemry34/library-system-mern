baf93268e06d2ba42d85d67e81a7227b73053616
srhqmp <ballanosarah@gmail.com>
2021-07-03 21:08:11 +0800

    reserve - first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,56 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Login from './Components/Login/Login';
-import Nav from './Components/Nav/Nav';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Books from './Components/Books/Books';
-import Borrow from './Components/Borrow/Borrow';
-import Request from './Components/Request/Request';
-import UserManagement from './Components/UserManagement/UserManagement';
-
+import Login from './Components/Login/Login'
+import Nav from './Components/Nav/Nav'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Books from './Components/Books/Books'
+import Borrow from './Components/Borrow/Borrow'
+import Reserve from './Components/Reserve/Reserve'
+import Request from './Components/Request/Request'
+import UserManagement from './Components/UserManagement/UserManagement'
 
 // import Dashboard from './Components/Dashboard/Das1board'
-
 
 export default function App() {
   return (
     <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
         <Switch>
-          <Route path="/" exact component={Login} />
-                <Switch>
-                  <Route path="/dashboard" exact >
-                    <Nav>
-                      <Dashboard />
-                    </Nav>
-                  </Route>
-                  <Route path="/books" exact >
-                    <Nav>
-                      <Books />
-                    </Nav>
-                  </Route>
-                  <Route path="/borrow" exact >
-                    <Nav>
-                      <Borrow />
-                    </Nav>
-                  </Route>
-                  <Route path="/requests" exact >
-                    <Nav>
-                      <Request />
-                    </Nav>
-                  </Route>
-                  <Route path="/users" exact >
-                    <Nav>
-                      <UserManagement />
-                    </Nav>
-                  </Route>
-                </Switch>
+          <Route path="/dashboard" exact>
+            <Nav>
+              <Dashboard />
+            </Nav>
+          </Route>
+          <Route path="/books" exact>
+            <Nav>
+              <Books />
+            </Nav>
+          </Route>
+          <Route path="/borrow" exact>
+            <Nav>
+              <Borrow />
+            </Nav>
+          </Route>
+          <Route path="/reserve" exact>
+            <Nav>
+              <Reserve />
+            </Nav>
+          </Route>
+          <Route path="/requests" exact>
+            <Nav>
+              <Request />
+            </Nav>
+          </Route>
+          <Route path="/users" exact>
+            <Nav>
+              <UserManagement />
+            </Nav>
+          </Route>
         </Switch>
-    </Router> 
-    
+      </Switch>
+    </Router>
   )
 }
