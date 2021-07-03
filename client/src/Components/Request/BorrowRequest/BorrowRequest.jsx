ab89253e7cf656beddab67ed0c468b1499400a1f
Gemry Bulante <bgemry@yahoo.com>
2021-07-03 20:51:17 +0800

    usermanage ment and return book(cherry pick)(tested)

--- client/src/Components/Request/BorrowRequest/BorrowRequest.jsx
+++ client/src/Components/Request/BorrowRequest/BorrowRequest.jsx
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import SwipeableViews from 'react-swipeable-views'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import Box from '@material-ui/core/Box'
-import {
-  fetchPending,
-  fetchApproved,
-  fetchCancell,
-} from './../../../Api/Borrower/Borrower'
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import {fetchApproved, fetchReturned} from './../../../Api/Borrower/Borrower'
 import BorrowData from './BorrowData/BorrowData'
 import { checkToken } from '../../../Api/Users/Users'
 import { useHistory } from 'react-router'
@@ -56,50 +52,30 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export default function ReservationRequest() {
-  const classes = useStyles()
-  const theme = useTheme()
-  const [value, setValue] = React.useState(0)
-  const [pendings, setPendings] = useState([])
-  const [approves, setApproved] = useState([])
-  const [cancels, setCancels] = useState([])
-  const [user, setUser] = useState(null)
-  const history = useHistory()
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  const [approves, setApproved] = useState([]);
+  const [returned, setReturned] = useState([]);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const res = await checkToken()
-      if (res === undefined) history.push('/')
-      else if (res.status === 401) history.push('/')
-      setUser(res.data)
-    }
-    fetchUser()
-  }, [history])
-
-  useEffect(() => {
-    let isCancelled = false
-    const filterStudents = (data) => {
-      return data.filter((data) =>
-        user.userType === 'student'
-          ? data.user.username === user.username
-          : true
-      )
-    }
-    const fetchApi = async () => {
-      let pendingData = await fetchPending()
-      let approvedData = await fetchApproved()
-      let cancellData = await fetchCancell()
-      if (!isCancelled) {
-        setPendings(filterStudents(pendingData))
-        setApproved(filterStudents(approvedData))
-        setCancels(filterStudents(cancellData))
-      }
+  useEffect(()=>{
+    let isCancelled = false;
+    
+    const fetchApi = async () =>{
+        let approvedData = await fetchApproved();
+        let returnedData = await fetchReturned();
+        console.log(returnedData);
+        if(!isCancelled){
+          setApproved(approvedData);
+          setReturned(returnedData)
+        }
     }
     try {
       fetchApi()
     } catch (e) {}
 
     return () => (isCancelled = true)
-  }, [user])
+  }, [])
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
@@ -120,9 +96,8 @@ export default function ReservationRequest() {
           variant="fullWidth"
           aria-label="full width tabs example"
         >
-          <Tab label="Pending" {...a11yProps(0)} />
-          <Tab label="Approved" {...a11yProps(1)} />
-          <Tab label="Cancelled" {...a11yProps(2)} />
+          <Tab label="Approved" {...a11yProps(0)} />
+          <Tab label="Returned" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
       <SwipeableViews
@@ -131,26 +106,12 @@ export default function ReservationRequest() {
         onChangeIndex={handleChangeIndex}
       >
         <TabPanel value={value} index={0} dir={theme.direction}>
-          <BorrowData
-            borrows={pendings}
-            status={'pending'}
-            userType={user && user.userType}
-          />
+            <BorrowData data={[approves, setApproved, setReturned]} status={'approved'} />
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
-          <BorrowData
-            borrows={approves}
-            status={'approved'}
-            userType={user && user.userType}
-          />
+            <BorrowData data={[returned, setApproved, setReturned]} status={'returned'} />
         </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          <BorrowData
-            borrows={cancels}
-            status={'cancelled'}
-            userType={user && user.userType}
-          />
-        </TabPanel>
+
       </SwipeableViews>
     </div>
   )
